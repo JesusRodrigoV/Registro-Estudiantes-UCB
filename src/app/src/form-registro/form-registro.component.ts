2e6e3f66dc41f70e15a2ef33b43085bfbada3091
JesusRodrigoV <jaicelvelasco105@gmail.com>
2025-02-04 09:56:49 -0400

    Componentes agregados

---
+++ src/app/src/form-registro/form-registro.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-registro',
+  imports: [],
+  templateUrl: './form-registro.component.html',
+  styleUrl: './form-registro.component.scss'
+})
+export class FormRegistroComponent {
+
+}
